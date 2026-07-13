6942e450cc240b60ac8c5e352336b628665bb924
axu1-eng <axu1@haverford.edu>
2026-07-13 16:39:45 -0400

    Seventh attempt adding Accessibility Regression Tests

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './lhci-root',
+      staticDistDir: './_site',
       url: [
         "/modernCSS/home",
         "/modernCSS/Projects/projects"
