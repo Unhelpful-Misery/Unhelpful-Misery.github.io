417138b84cc7d5de79c846374787a822a876724e
axu1-eng <axu1@haverford.edu>
2026-07-13 16:27:33 -0400

    Third attempt adding Accessibility Regression Tests

---
+++ lighthouserc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './lhci-root',
+      url: [
+        "/modernCSS/home.html",
+        "/modernCSS/Projects/projects.html"
+      ],
+      numberOfRuns: 1,
+    },
+    assert: {
+      assertMatrix: [
+        {
+          matchingUrlPattern: '.*home\\.html$',
+          assertions: {
+            'categories:accessibility':['error', { minScore: 1.0 }]
+          }
+        },
+        {
+          matchingUrlPattern: '.*projects\\.html$',
+          assertions: {
+            'categories:accessibility':['error', { minScore: 1.0 }]
+          }
+        }
+      ]
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
